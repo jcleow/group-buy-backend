ab10f4e1b5cff796755b7fa4ff250651f7d3c453
Veena-S <veenasivan@gmail.com>
2021-02-25 08:39:21 +0800

    Add route for counting the number of purchases per listing

--- controllers/purchases.mjs
+++ controllers/purchases.mjs
@@ -3,7 +3,20 @@ export default function initPurchasesController(db) {
     res.send();
   };
 
+  /**
+   * Function to handle the request to count the number of purchases for a listing
+   * @param request
+   * @param response
+   */
+  const countPurchasesPerListing = async (request, response) => {
+    db.Purchase.count({ where: { listing_id: request.params.listingId } })
+      .then((purchaseCount) => {
+        response.status(200).send({ purchaseCount });
+      })
+      .catch((error) => console.log(error));
+  };
+
   return {
-    index,
+    index, countPurchasesPerListing,
   };
 }
